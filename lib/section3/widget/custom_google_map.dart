2ad88b922ad4979adef28f7b5d9bdc18a0327412
King <jafaar.abdulkareem@gmail.com>
2025-04-10 18:33:01 +0530

    controler change latLng

--- lib/section3/widget/custom_google_map.dart
+++ lib/section3/widget/custom_google_map.dart
@@ -11,16 +11,51 @@ class CustomGoogleMap extends StatefulWidget {
 
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition controllerInitialCamerPosition;
+  late GoogleMapController googleMapController;
   @override
   void initState() {
     super.initState();
   }
 
   @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const GoogleMap(
-        initialCameraPosition: CameraPosition(
-        
-            target: LatLng(13.046279282623589, 77.59288321390162)));
+    return Stack(
+      children: [
+        GoogleMap(
+          zoomControlsEnabled: false,
+          onMapCreated: (controller) {
+            googleMapController = controller;
+          },
+          initialCameraPosition: const CameraPosition(
+            zoom: 12,
+            target: LatLng(
+              13.046279282623589,
+              77.59288321390162,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 16,
+          left: 16,
+          right: 16,
+          child: ElevatedButton(
+            onPressed: () {
+              googleMapController.animateCamera(
+                CameraUpdate.newLatLng(
+                  const LatLng(12.320505273066722, 76.62978062497629),
+                ),
+              );
+            },
+            child: const Text("Mysuru"),
+          ),
+        )
+      ],
+    );
   }
 }
